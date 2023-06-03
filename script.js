8ceca557fd64088fb530fbece4b3f3b368a2fd11
Chifundoh <fchimzondo@gmail.com>
2023-06-03 11:42:46 +0200

    modified

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Coder", "Data Scientist", "Data Miner", "Data Analysts", "Developer", "Blogger", " Post Designer", "Freelancer"],
+        strings: ["Coder", "Developer", "Blogger", " Post Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Coder", "Data Scientist", "Data Miner", "Data Analysts", "Developer", "Blogger", "Post Designer", "Freelancer"],
+        strings: ["Coder",  "Developer", "Blogger", "Post Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
